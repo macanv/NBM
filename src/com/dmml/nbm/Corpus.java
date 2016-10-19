faca5b8e6e802eb68263fc96c06a44e4d0f257ee
389631699@qq.com <389631699@qq.com>
2016-10-19 22:43:00 +0800

    Initial commit

---
+++ src/com/dmml/nbm/Corpus.java
@@ -0,0 +1,11 @@
+package com.dmml.nbm;
+
+/**
+ * Created by macan on 2016/10/19.
+ */
+
+/**
+ * 语料库处理
+ */
+public class Corpus {
+}
---
+++ src/com/dmml/test/testNBM.java
@@ -0,0 +1,18 @@
+package com.dmml.test;
+
+/**
+ * Created by macan on 2016/10/19.
+ */
+
+import org.junit.Test;
+
+/**
+ * NBM test
+ */
+public class testNBM {
+
+    @Test
+    public  void  testModel(){
+
+    }
+}
