cba19ddd11d50311d1afbd38f0245ac62cd9d4ea
macan <389631699@qq.com>
2016-11-04 15:34:09 +0800

    Laplacian correction Navie Bayes Model

--- src/com/dmml/nbm/NavieBayesModel.java
+++ src/com/dmml/nbm/NavieBayesModel.java
@@ -1,14 +1,21 @@
 package com.dmml.nbm;
 
 import javax.print.Doc;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 /**
  * Created by macan on 2016/11/4.
  */
 public class NavieBayesModel {
     private int numberOfSpam;
-    private int getNumberOfHam;
+    private int numberOfHam;
 
     private Map<String, Integer> spamCountMap;
     private Map<String, Integer> hamCountMap;
@@ -27,20 +34,99 @@ public class NavieBayesModel {
     public NavieBayesModel(Corpus corpus, int numberOfCategort){
         this.numberOfCategort = numberOfCategort;
         numberOfSpam = corpus.getNumberOfSpam();
-        getNumberOfHam = corpus.getNumberOfHam();
+        numberOfHam = corpus.getNumberOfHam();
 
         spamCountMap = corpus.getSpanTermCountMap();
         hamCountMap = corpus.getHamTermCountMap();
 
-        spamPrior = (numberOfSpam + 1) / (numberOfSpam + getNumberOfHam + numberOfCategort);
-        hamPrior = (getNumberOfHam + 1) / (numberOfSpam + getNumberOfHam + numberOfCategort);
+        spamPrior = ((numberOfSpam + 1) * 1.0) / (numberOfSpam + numberOfHam + numberOfCategort);
+        hamPrior = ((numberOfHam + 1) * 1.0) / (numberOfSpam + numberOfHam + numberOfCategort);
+
+        spamProbilityMap = new HashMap<>();
+        hamProbilityMap = new HashMap<>();
     }
 
 
     /**
      * 训练垃圾邮件
      */
-    public void tarinSpam(){
+    public void run(){
+        double p_span = 0.0;
+        double p_ham = 0.0;
+        for (Map.Entry<String, Integer> entry : spamCountMap.entrySet()){
+            String word = entry.getKey();
+            int  countOfSpam = entry.getValue();
+            int N_i = 1;
+            //如果单词在正常邮件中也出现过，那么Ni = 2,否则为1
+            if (hamCountMap.containsKey(word)){
+                N_i++;
+            }
+            p_span = ((countOfSpam + 1) * 1.0) / (numberOfSpam + N_i);
+            spamProbilityMap.put(word, p_span);
+        }
 
+        for (Map.Entry<String, Integer> entry : hamCountMap.entrySet()){
+            String word = entry.getKey();
+            int countOfHam = entry.getValue();
+            int N_i = 1;
+            if (spamCountMap.containsKey(word)){
+                N_i++;
+            }
+            p_ham = ((countOfHam + 1 ) *1.0) / (numberOfHam + N_i);
+            hamProbilityMap.put(word, p_ham);
+        }
+    }
+
+
+    /**
+     * 判断一封邮件是否是垃圾邮件
+     * @param path 需要判断邮件的地址
+     * @return  垃圾邮件返回true, 否则返回false
+     */
+    public boolean isSpam(String path) {
+        boolean isSpam = false;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(new File(path)));
+            String line;
+            if ((line = reader.readLine()) != null) {
+                StringTokenizer tokenizer = new StringTokenizer(line);
+                ArrayList<String> doc = new ArrayList<>();
+                double p_spam = 1.0;
+                double p_ham = 1.0;
+                while (tokenizer.hasMoreTokens()) {
+                    String word = tokenizer.nextToken();
+                    if (word.equals("spam"))
+                    doc.add(word);
+                    //计算属于哪一个邮件的probability
+                    //如果该单词出现在垃圾邮件的字典中
+                    if (spamCountMap.containsKey(word)) {
+                        p_spam *= spamProbilityMap.get(word);
+                    } else {
+                        p_spam *= (1 * 1.0) / (numberOfSpam + 1);
+                    }
+                }
+
+
+                p_spam *= spamPrior;
+                for (String word : doc) {
+                    if (hamCountMap.containsKey(word)) {
+                        p_ham *= hamProbilityMap.get(word);
+                    } else {
+                        p_ham *= (1 * 1.0) / (numberOfHam + 1);
+                    }
+                }
+                p_ham *= hamPrior;
+
+                System.out.println("p_spam = " + p_spam + "    p_ham = " + p_ham);
+                if (p_spam > p_ham) {
+                    isSpam = true;
+                } else {
+                    isSpam = false;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return isSpam;
     }
 }
--- src/com/dmml/test/testNBM.java
+++ src/com/dmml/test/testNBM.java
@@ -4,6 +4,8 @@ package com.dmml.test;
  * Created by macan on 2016/10/19.
  */
 
+import com.dmml.nbm.Corpus;
+import com.dmml.nbm.NavieBayesModel;
 import org.junit.Test;
 
 /**
@@ -13,6 +15,13 @@ public class testNBM {
 
     @Test
     public  void  testModel(){
+        String dataPath = "emails/training/SMSCollection.txt";
+        String textPath = "emails/test/test.txt";
+        Corpus corpus = new Corpus(dataPath);
+        NavieBayesModel model = new NavieBayesModel(corpus, 2);
+        model.run();
 
+        boolean isspam = model.isSpam(textPath);
+        System.out.println("the mail is spam?  " + isspam);
     }
 }
